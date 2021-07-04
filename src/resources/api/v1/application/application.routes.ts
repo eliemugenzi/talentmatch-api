3ca0727256f5ae21419a06e70795d0cf4374ba94
Elie Mugenzi <eliemugenzi@gmail.com>
2021-07-04 20:20:41 +0200

    Approve, reject and view an application

--- src/resources/api/v1/application/application.routes.ts
+++ src/resources/api/v1/application/application.routes.ts
@@ -1,9 +1,23 @@
 import { Router } from 'express';
 import checkAuth from 'middlewares/checkAuth';
-import { getMany } from './application.controller';
+import { changeStatus, checkApplication, getMany, getOne } from './application.controller';
 
 const router = Router();
 
 router.get('/', checkAuth({ roles: ['hr'] }), getMany);
+router.get('/:id', checkAuth({ roles: ['hr'] }), checkApplication, getOne);
+router.put(
+  '/:id/approve',
+  checkAuth({ roles: ['hr'] }),
+  checkApplication,
+  changeStatus({ status: 'passed' }),
+);
+
+router.put(
+  '/:id/reject',
+  checkAuth({ roles: ['hr'] }),
+  checkApplication,
+  changeStatus({ status: 'dropped' }),
+);
 
 export default router;
